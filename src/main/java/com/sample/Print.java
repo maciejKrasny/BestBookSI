1910d9e7effde383ba9c52951c6847eefe9a2f6b
Unknown <maciej.krasny97@gmail.com>
2018-12-16 21:12:42 +0100

    Small changes

--- src/main/java/com/sample/Print.java
+++ src/main/java/com/sample/Print.java
@@ -27,18 +27,20 @@ public class Print {
 	public static void showQuestion(Question question) {
 		JPanel panel = new JPanel();
 	    panel.setLayout(new GridLayout(question.getAnswerOptions().size()+1, 1));
-	    ButtonGroup bGroup = new ButtonGroup();
+	    
+	    ButtonGroup buttonsGroup = new ButtonGroup();
+	    
 	    JLabel questionText = new JLabel();
 	    questionText.setText(question.getText());
 	    panel.add(questionText);
 	    
 	    for (String answer : question.getAnswerOptions()) {
             JRadioButton button = new JRadioButton(answer);
-            bGroup.add(button);
+            buttonsGroup.add(button);
             panel.add(button);
         }
 	    
-	    Integer n = JOptionPane.showOptionDialog(
+	    Integer option = JOptionPane.showOptionDialog(
                 null,
                 panel,
                 "Books options",
@@ -49,8 +51,8 @@ public class Print {
                 null
         );
 	    
-	    if (n.equals(0)) {
-            for (Enumeration<AbstractButton> buttons = bGroup.getElements(); buttons.hasMoreElements(); ) {
+	    if (option.equals(0)) {
+            for (Enumeration<AbstractButton> buttons = buttonsGroup.getElements(); buttons.hasMoreElements(); ) {
                 AbstractButton button = buttons.nextElement();
 
                 if (button.isSelected()) {
@@ -68,16 +70,22 @@ public class Print {
 	public static void result(List<String> books){
 		
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(books.size()+1,1));
-        System.out.println(books);
-        JLabel label = new JLabel("Books for you: ");
-        label.setFont(new Font("Serif", Font.BOLD, 20));
-        panel.add(label);
+        if (books.get(0) != "Get out if you can't decide!") {
+        	
+	        panel.setLayout(new GridLayout(books.size()+1,1));
+	        
+	        JLabel label = new JLabel("Books for you: ");
+	        label.setFont(new Font("Serif", Font.BOLD, 20));
+	        panel.add(label);
+        }
+        else {
+        	panel.setLayout(new GridLayout(1,1));
+	        
+        }
+        
         for (int i=0; i<books.size(); i++) {
         	panel.add(new JLabel(books.get(i)));
         }
-        
-
         JOptionPane.showOptionDialog(null,panel,"Books",JOptionPane.CLOSED_OPTION,JOptionPane.PLAIN_MESSAGE,
         		null,
                 null,
